bb52f2147154ab9a8d4040da84e337940da4bb9f
christopherhanke <christopherhanke@googlemail.com>
2020-09-10 19:36:23 +0200

    added some extra moods to reply to.

--- mood.py
+++ mood.py
@@ -20,6 +20,10 @@ if mood == "happy":
     str_answer = "It is great to see you happy!"
 elif mood == "nervous":
     str_answer = "Take a deep breath 3 times!"
+elif mood == "sad":
+    str_answer = "Oh, I'm sorry. Here, take a cookie."
+elif mood == "relaxed":
+    str_answer = "That's nice. Sweet and calm, wins the day."
 else:
     str_answer = "I don't recognize this mood. Sorry."
 
